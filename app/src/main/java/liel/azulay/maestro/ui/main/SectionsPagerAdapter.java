151f31f7e0e547ccd0dfaae4d86f9e01ce219c7b
Liel Azulay <liel999999@gmail.com>
2022-09-05 17:49:02 +0300

    mic recording permission

--- app/src/main/java/liel/azulay/maestro/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/liel/azulay/maestro/ui/main/SectionsPagerAdapter.java
@@ -1,19 +1,13 @@
 package liel.azulay.maestro.ui.main;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import liel.azulay.maestro.R;
-
 
 public class SectionsPagerAdapter extends FragmentStateAdapter
 {
-
-    @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
 
     public SectionsPagerAdapter(@NonNull FragmentActivity fragmentActivity)
     {
